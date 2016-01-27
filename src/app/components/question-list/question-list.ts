8018379edf3c4ecee21f73656a85d2cdea17b2b1
Jeff Cross <middlefloor@gmail.com>
2016-01-26 17:22:07 -0800

    feat: add question-list component

---
+++ src/app/components/question-list/question-list.ts
@@ -0,0 +1,16 @@
+import {Component} from 'angular2/core';
+
+
+@Component({
+  selector: 'question-list',
+  templateUrl: 'app/components/question-list/question-list.html',
+  styleUrls: ['app/components/question-list/question-list.css'],
+  providers: [],
+  directives: [],
+  pipes: []
+})
+export class QuestionList {
+
+  constructor() {}
+
+}
